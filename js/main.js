6d8e81521ec449a7c4992f00b0d94c9bf4bb80b7
Ali-Khaled-dev <ali.khaled.dev@gmail.com>
2024-05-20 15:42:51 +0300

    end retouch

--- js/main.js
+++ js/main.js
@@ -1,124 +1,96 @@
-
 var scroolbottoun = $("#scroll-top");
 
 $(function () {
+  // display none for loading page
 
-    // display none for loading page
+  setTimeout(function () {
+    document.querySelector(".preloader").classList.add("d-none");
+  }, 3000);
 
-    setTimeout(function () {
+  // Scroll Page for click navBar
 
-        document.querySelector('.preloader').classList.add('d-none');
-    }, 3000);
+  $("nav li a").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#" + $(this).data("scroll")).offset().top,
+      },
+      100
+    );
+  });
 
+  // add active class for element <a>
 
-    // Scroll Page for click navBar
+  $("nav li a").click(function () {
+    $("nav li a").removeClass("active");
+    $(this).addClass("active");
+  });
 
-    $('nav li a').click(function () {
+  $(window).scroll(function () {
+    // Synchronization of links with the site
 
-        $('html, body').animate({
-
-            scrollTop: $('#' + $(this).data('scroll')).offset().top
-        }, 100);
+    $(".block").each(function () {
+      if ($(window).scrollTop() >= $(this).offset().top - 10) {
+        $("nav li a").removeClass("active");
+        var blocID = $(this).attr("id");
+        $('nav li a[data-scroll="' + blocID + '"]').addClass("active");
+      }
     });
 
+    // Back to top button show/hide
 
-    // add active class for element <a>
+    $(window).scrollTop() >= 300
+      ? scroolbottoun.fadeIn(200)
+      : scroolbottoun.fadeOut(200);
 
-    $('nav li a').click(function () {
-        $('nav li a').removeClass('active');
-        $(this).addClass('active');
+    // animate num skills
+  });
+
+  scroolbottoun.click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
+  $(window).on("scroll", function () {
+    $(".num").each(function () {
+      var $this = $(this);
+      var countTo = $this.attr("data-width");
+
+      if (window.scrollY >= $this.offset().top - window.innerHeight) {
+        $({ countNum: $this.text() }).animate(
+          {
+            countNum: countTo,
+          },
+          {
+            duration: 3000,
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(countTo);
+            },
+          }
+        );
+      }
     });
+  });
 
+  function hundlemenue() {
+    $("#menu").toggleClass("is-active");
+    $("#actions").toggleClass("is-active");
+  }
+  $("#menu").click(function () {
+    hundlemenue();
+  });
 
-
-    $(window).scroll(function () {
-
-        // Synchronization of links with the site
-
-        $('.block').each(function () {
-            if ($(window).scrollTop() >= $(this).offset().top - 10) {
-                $('nav li a').removeClass('active');
-                var blocID = $(this).attr('id');
-                $('nav li a[data-scroll="' + blocID + '"]').addClass('active');
-            }
-        });
-
-        // Back to top button show/hide
-
-        $(window).scrollTop() >= 300 ? scroolbottoun.fadeIn(200) : scroolbottoun.fadeOut(200);
-
-        // animate num skills
-
-
-    });
-
-    scroolbottoun.click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 800);
-        return false;
-    });
-
-    // animate progress bar for skills section
-
-    $('.progress .progress-bar').each(function () {
-        $(this).animate({
-            width: $(this).attr('data-width') + '%'
-        }, 5000);
-    });
-
-
-    // var nums = document.querySelectorAll(".num");
-    // var section = document.querySelector('.skills');
-
-    $(window).on('scroll', function () {
-        $('.num').each(function () {
-            var $this = $(this);
-            var countTo = $this.attr('data-width');
-
-            if (window.scrollY >= $this.offset().top - window.innerHeight) {
-                $({ countNum: $this.text() }).animate({
-                    countNum: countTo
-                },
-                    {
-                        duration: 3000,
-                        // easing: 'linear',
-                        step: function () {
-                            $this.text(Math.floor(this.countNum));
-                        },
-                        complete: function () {
-                            $this.text(countTo);
-                        }
-                    });
-            }
-        });
-    });
-
-
-
-
-    $('.show-popup').click(function () {
-        $('.poupup').fadeIn();
-    })
-    $('.poupup button').click(function () {
-        $('.poupup').fadeOut();
-    })
-
-    function hundlemenue() {
-        $("#menu").toggleClass("is-active");
-        $("#actions").toggleClass("is-active");
+  // animate progress bar for skills section
+  $(".progress .progress-bar").each(function () {
+    if (window.scrollY >= $this.offset().top - $(window).highet()) {
+      $this.animate(
+        {
+          width: $(this).attr("data-width") + "%",
+        },
+        5000
+      );
     }
-    $("#menu").click(function () {
-        hundlemenue();
-    });
-
-
-    // $('.work-images .overlay').hover(function () {
-    //     $(this).turnOnOverlay();
-    // }, function () {
-    //     $(this).turnOffOverlay();
-    // })
-
+  });
 });
-
-
-
-
